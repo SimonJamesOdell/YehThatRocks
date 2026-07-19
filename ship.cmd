b08d4e530f2e4fdc6273340a024ec22d9ea52ed2
Simon James Odell <simonjamesodell@live.co.uk>
2026-07-19 15:53:42 +0100

    fix: remove SHIP_ARGS pass-through to eliminate garbled-token error after deploy; sync fast.cmd -SkipVerifyGate

--- ship.cmd
+++ ship.cmd
@@ -11,12 +11,6 @@ if /I "%~1"=="fast" (
 
 set "SHIP_PASSWORD=%~1"
 if not "%~1"=="" shift
-set "SHIP_ARGS="
-:collect_args
-if "%~1"=="" goto run_ship
-set "SHIP_ARGS=%SHIP_ARGS% "%~1""
-shift
-goto collect_args
 
 :run_ship
 set "SHIP_FLAGS="
@@ -68,6 +62,6 @@ if /I "%SHIP_MODE%"=="fast" (
 )
 
 :invoke_ship
-pwsh -NoProfile -ExecutionPolicy Bypass -File "%~dp0deploy\ship-local.ps1" -ShipPassword "%SHIP_PASSWORD%" %SHIP_FLAGS% %SHIP_ARGS%
+pwsh -NoProfile -ExecutionPolicy Bypass -File "%~dp0deploy\ship-local.ps1" -ShipPassword "%SHIP_PASSWORD%" %SHIP_FLAGS%
 set "SHIP_EXIT=%ERRORLEVEL%"
 endlocal & exit /b %SHIP_EXIT%
